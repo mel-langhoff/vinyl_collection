04768a8ed90fde60db7ed681a64b037e57394d18
Mel Langhoff <mlanghoff@uwalumni.com>
2024-03-15 20:30:18 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/", to: "artists#index"
+
   resources :artists, only: [:index, :show]
 
-  root "artists#index"
+  # root "artists#index"
 end
